3062bc06507fe71a05f46a1d382764fa281a39db
akcays <ilkesedefakcay@gmail.com>
2017-05-20 16:25:37 -0700

    Update methods to handle multiple colors

--- highlight.js
+++ highlight.js
@@ -1,12 +1,13 @@
 chrome.storage.local.get('changes', function(results) {
+  console.log(results.changes);
   var changes = results.changes;
-  
+
   changes.textToUnhighlight.forEach((value) => {
-    $('body').unhighlight(value);
+    $('body').unhighlight(value, {className: `highlight-${value.color}`});
   });
-  
+
   changes.textToHighlight.forEach((value) => {
-    $('body').highlight(value);
+    $('body').highlight(value.note, {className: `highlight-${value.color}`});
   });
 
   chrome.storage.local.remove('changes');
